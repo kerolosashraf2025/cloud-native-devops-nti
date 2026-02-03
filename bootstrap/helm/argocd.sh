5ff8efc0306b4d719795c97fba9e16c780e2c15a
Kerolos Ashraf <kerolosashraf2025@gmail.com>
2026-02-03 13:58:19 -0500

    Add bootstrap structure for EKS (ingress-nginx + argocd)

---
+++ bootstrap/helm/argocd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+
+helm upgrade --install argocd argo/argo-cd \
+  --namespace argocd \
+  --create-namespace
---
+++ bootstrap/helm/ingress-nginx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx \
+  --create-namespace
