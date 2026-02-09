4924653334c6030ceb90c904f994c2b655d60a5d
Kerolos Ashraf <kerolosashraf2025@gmail.com>
2026-02-09 17:01:28 -0500

    Expose ArgoCD UI via LoadBalancer

--- bootstrap/helm/argocd.sh
+++ bootstrap/helm/argocd.sh
@@ -6,4 +6,11 @@ helm repo update
 
 helm upgrade --install argocd argo/argo-cd \
   --namespace argocd \
-  --create-namespace
+  --create-namespace \
+  --set server.service.type=LoadBalancer
+
+echo "Waiting for ArgoCD server to be ready..."
+kubectl rollout status deployment/argocd-server -n argocd --timeout=600s
+
+echo "ArgoCD service info:"
+kubectl get svc -n argocd
